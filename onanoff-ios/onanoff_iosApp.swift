bb8937421c4fc253c1c6ddca4d473a4fd9bb623e
Van Huy <111716590+huyvan2001@users.noreply.github.com>
2023-11-14 23:40:55 +0700

    Initial Commit

---
+++ onanoff-ios/onanoff_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  onanoff_iosApp.swift
+//  onanoff-ios
+//
+//  Created by Lê Văn Huy on 14/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct onanoff_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
